4e1355debef99d4c617e383ff22fb038b6bfd831
lorranhenrique <logplayviews@gmail.com>
2023-11-08 15:54:30 -0300

    Ajustes na exclusão de filas

--- FilaSenha.c
+++ FilaSenha.c
@@ -1,11 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
 
 typedef struct No {
     char *senha;
     struct No *prox;
 } No;
+
+
+No *push(No *atual, int *t);
+No *pop(No *atual);
+void print(No *atual);
+
+
+int main() {
+
+    setlocale(LC_ALL,"");
+
+    No *fila = NULL;
+    int op, tam = 0;
+
+
+    printf("Seja bem vindo ao sistema do Mec-Dunud\n");
+
+    do {
+        printf("\n\nGostaria de fazer um pedido? \n1 - Gerar senha\n2 - Atender usuario\n3 - Imprimir \n0 - Sair do sistema\n\n");
+        scanf("%d", &op);
+
+        switch (op) {
+            case 1:
+                fila = push(fila, &tam);
+                break;
+            case 2:
+                if(fila==NULL){
+                    printf("Lista vazia");
+                }else{
+                    fila = pop(fila);
+                }
+                break;
+            case 3:
+                print(fila);
+                break;
+            case 0:
+                break;
+            default:
+                printf("Valor inválido, escolha um de 0 - 3\n");
+                break;
+        }
+    } while (op != 0);
+
+
+    while (fila != NULL) {
+        No *temp = fila;
+        fila = fila->prox;
+        free(temp->senha);
+        free(temp);
+    }
+
+    return 0;
+}
 
 No *push(No *atual, int *t) {
     No *novo = (No *)malloc(sizeof(No));
@@ -20,71 +74,35 @@ No *push(No *atual, int *t) {
 
 No *pop(No *atual) {
 
-    No *temp,*inicio =(No*)malloc(sizeof(No));
-    if (atual == NULL) {
-        printf("Lista vazia\n");
-    } else {
-        inicio = atual;
-        temp = atual;
+    No *inicio=(No*)malloc(sizeof(No));
+    inicio=atual;
+    No *temp = NULL;
+
+    if(atual==NULL){
+        return atual;
+    }else{
         while(atual->prox!=NULL){
+            temp=atual;
             atual=atual->prox;
         }
-        while(temp->prox!=atual){
-            temp=temp->prox;
+        if(atual->prox==NULL&&temp==NULL){
+            atual=atual->prox;
+            return atual;
+        }else{
+            temp->prox=atual->prox;
+            free(atual);
+            return inicio;
         }
-        temp->prox=NULL;
-        atual=inicio;
     }
-    return atual;
 }
 
 void print(No *atual) {
-    int c = 0;
     if (atual == NULL) {
         printf("Lista vazia\n");
     } else {
         while (atual != NULL) {
-            printf("Senha %d: %s\n", c + 1, atual->senha);
+            printf("Senha: %s\n", atual->senha);
             atual = atual->prox;
-            c++;
         }
     }
-}
-int main() {
-    No *fila = NULL;
-    int op, tam = 0;
-
-    printf("Seja bem vindo ao sistema do Mec-Dunud\n");
-
-    do {
-        printf("Gostaria de fazer um pedido? \n0 - Sair\n1 - Inserir\n2 - Remover\n3 - Imprimir\n ");
-        scanf("%d", &op);
-
-        switch (op) {
-            case 1:
-                fila = push(fila, &tam);
-                break;
-            case 2:
-                fila = pop(fila);
-                break;
-            case 3:
-                print(fila);
-                break;
-            case 0:
-                break;
-            default:
-                printf("Valor inválido, escolha um de 0 - 3\n");
-                break;
-        }
-    } while (op != 0);
-
-    
-    while (fila != NULL) {
-        No *temp = fila;
-        fila = fila->prox;
-        free(temp->senha);
-        free(temp);
-    }
-
-    return 0;
 }
